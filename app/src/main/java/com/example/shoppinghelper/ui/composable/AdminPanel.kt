689216663baf91b002b03d5ace2aefd6e088c327
Milosz Matuszewski <myyylek@gmail.com>
2023-05-20 19:46:06 +0200

    created admin panel screen

---
+++ app/src/main/java/com/example/shoppinghelper/ui/composable/AdminPanel.kt
@@ -0,0 +1,13 @@
+package com.example.shoppinghelper.ui.composable
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+
+@Composable
+fun AdminPanel(
+    navController: NavController
+){
+    Text(text = "Admin Panel")
+}
