e200898840f6ad6431221a2205af55039d90d413
Milosz Matuszewski <myyylek@gmail.com>
2023-05-20 13:56:52 +0200

    main activity schema updated

--- app/src/main/java/com/example/shoppinghelper/MainActivity.kt
+++ app/src/main/java/com/example/shoppinghelper/MainActivity.kt
@@ -6,13 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.shoppinghelper.ui.theme.ShoppingHelperTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    ShoppingHelperApp()
                 }
             }
         }
