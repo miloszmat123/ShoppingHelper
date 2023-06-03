a04973182c062fff6321384e6bd981c466378aaf
Milosz Matuszewski <myyylek@gmail.com>
2023-06-03 22:16:29 +0200

    updated container

--- app/src/main/java/com/example/shoppinghelper/data/AppContainer.kt
+++ app/src/main/java/com/example/shoppinghelper/data/AppContainer.kt
@@ -5,9 +5,14 @@ import com.example.shoppinghelper.auth.GoogleAuthUiClient
 import com.google.android.gms.auth.api.identity.Identity
 
 
+
 class AppContainer(private val context: Context) {
 
     val googleAuthUiClient: GoogleAuthUiClient by lazy {
         GoogleAuthUiClient(context, Identity.getSignInClient(context))
+    }
+
+    val productDao: ProductDao by lazy {
+        ShoppingHelperDatabase.getDatabase(context).productDao()
     }
 }
