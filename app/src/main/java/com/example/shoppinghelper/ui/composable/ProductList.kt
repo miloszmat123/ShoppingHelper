7b45008607f715ee0074fe56a9e17869dde09694
Milosz Matuszewski <myyylek@gmail.com>
2023-06-09 13:33:15 +0200

    fix to print all info about the product

--- app/src/main/java/com/example/shoppinghelper/ui/composable/ProductList.kt
+++ app/src/main/java/com/example/shoppinghelper/ui/composable/ProductList.kt
@@ -1,7 +1,6 @@
 package com.example.shoppinghelper.ui.composable
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,8 +9,6 @@ import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.shoppinghelper.data.ViewModelProvider
 import com.example.shoppinghelper.products.ProductsViewModel
-import kotlinx.coroutines.flow.forEach
-import kotlinx.coroutines.flow.toList
 
 @Composable
 fun ProductList(
@@ -22,7 +19,7 @@ fun ProductList(
 
     val user = productsViewModel.user
     if (user != null) {
-        val products by  productsViewModel.getProducts(user.userId).collectAsState()
+        val products by  productsViewModel.getProductsByUserId(user.userId).collectAsState()
         Column() {
             Text(text = user.userId)
             Button(onClick = {
@@ -33,7 +30,7 @@ fun ProductList(
             }
             products.forEach(
                 action = {
-                    Text(text = it.productName)
+                    Text(text = it.productName + " " + it.productType + " " + it.nfcId)
                 }
             )
             Button(onClick = { navigate_add() }) {
