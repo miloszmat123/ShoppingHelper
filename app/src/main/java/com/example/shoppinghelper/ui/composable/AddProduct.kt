3e12ff2e734d895532890dc8b9da172f50aaea3a
Milosz Matuszewski <myyylek@gmail.com>
2023-06-14 00:37:20 +0200

    better back buttons

--- app/src/main/java/com/example/shoppinghelper/ui/composable/AddProduct.kt
+++ app/src/main/java/com/example/shoppinghelper/ui/composable/AddProduct.kt
@@ -1,13 +1,18 @@
 package com.example.shoppinghelper.ui.composable
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -39,52 +44,67 @@ fun AddProduct(
         var productName by remember { mutableStateOf("") }
         var productType by remember { mutableStateOf("") }
         var nfcId by remember { mutableStateOf("") }
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            TextField(
-                value = productName,
-                onValueChange = { productName = it },
-                label = { Text("productName") },
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 50.dp),
-                shape = RoundedCornerShape(20.dp),
-                colors = TextFieldDefaults.textFieldColors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = productName,
+                    onValueChange = { productName = it },
+                    label = { Text("productName") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 50.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
                 )
-            )
 
-            TextField(
-                value = productType,
-                onValueChange = { productType = it },
-                label = { Text("productType") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 15.dp),
-                shape = RoundedCornerShape(20.dp),
-                colors = TextFieldDefaults.textFieldColors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
+                TextField(
+                    value = productType,
+                    onValueChange = { productType = it },
+                    label = { Text("productType") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 15.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
                 )
-            )
 
-            Button(onClick = { nfcId = nfcMethods.writeNfcTag() ?: "" }) {
-                Text("Scan NFC")
-            }
-            if( nfcId != "") {
-                Button(onClick = {
-                    userProductsViewModel.addProduct(user.userId, productName, productType, nfcId)
-                    navigate()
-                })
-                {
-                    Text("Submit")
+                Button(onClick = { nfcId = nfcMethods.writeNfcTag() ?: "" }) {
+                    Text("Scan NFC")
                 }
+                if (nfcId != "") {
+                    Button(onClick = {
+                        userProductsViewModel.addProduct(
+                            user.userId,
+                            productName,
+                            productType,
+                            nfcId
+                        )
+                        navigate()
+                    })
+                    {
+                        Text("Submit")
+                    }
+                }
+            }
+            IconButton(
+                onClick = { navigate() },
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(16.dp)
+            ) {
+                Icon(Icons.Default.Close, contentDescription = "Close")
             }
         }
 
--- app/src/main/java/com/example/shoppinghelper/ui/composable/EditProduct.kt
+++ app/src/main/java/com/example/shoppinghelper/ui/composable/EditProduct.kt
@@ -1,13 +1,18 @@
 package com.example.shoppinghelper.ui.composable
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -42,55 +47,64 @@ fun EditProduct(
         var productName by remember { mutableStateOf(currentProduct.productName) }
         var productType by remember { mutableStateOf(currentProduct.productType) }
         val nfcId by remember { mutableStateOf(currentProduct.nfcId) }
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            TextField(
-                value = productName,
-                onValueChange = { productName = it },
-                label = { Text("productName") },
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 50.dp),
-                shape = RoundedCornerShape(20.dp),
-                colors = TextFieldDefaults.textFieldColors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
-                )
-            )
-
-            TextField(
-                value = productType,
-                onValueChange = { productType = it },
-                label = { Text("productType") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 15.dp),
-                shape = RoundedCornerShape(20.dp),
-                colors = TextFieldDefaults.textFieldColors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
-                )
-            )
-            if (nfcId != "") {
-                Button(onClick = {
-                    userProductsViewModel.updateProduct(
-                        currentProductId,
-                        user.userId,
-                        productName,
-                        productType,
-                        nfcId
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = productName,
+                    onValueChange = { productName = it },
+                    label = { Text("productName") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 50.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
                     )
-                    navigate()
-                })
-                {
-                    Text("Save")
+                )
+
+                TextField(
+                    value = productType,
+                    onValueChange = { productType = it },
+                    label = { Text("productType") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 15.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
+                if (nfcId != "") {
+                    Button(onClick = {
+                        userProductsViewModel.updateProduct(
+                            currentProductId,
+                            user.userId,
+                            productName,
+                            productType,
+                            nfcId
+                        )
+                        navigate()
+                    })
+                    {
+                        Text("Save")
+                    }
                 }
+            }
+            IconButton(
+                onClick = { navigate() },
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(16.dp)
+            ) {
+                Icon(Icons.Default.Close, contentDescription = "Close")
             }
         }
     }
