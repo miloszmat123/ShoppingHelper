c3a202e1f786dd59f62f02252cd0e08a896eaa11
Milosz Matuszewski <myyylek@gmail.com>
2023-06-14 00:30:04 +0200

    better ui for edit and adding

--- app/src/main/java/com/example/shoppinghelper/ui/composable/AddProduct.kt
+++ app/src/main/java/com/example/shoppinghelper/ui/composable/AddProduct.kt
@@ -1,16 +1,26 @@
 package com.example.shoppinghelper.ui.composable
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.shoppinghelper.data.ViewModelProvider
 import com.example.shoppinghelper.products.UserProductsViewModel
@@ -29,23 +39,42 @@ fun AddProduct(
         var productName by remember { mutableStateOf("") }
         var productType by remember { mutableStateOf("") }
         var nfcId by remember { mutableStateOf("") }
-
-        Column {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             TextField(
                 value = productName,
                 onValueChange = { productName = it },
-                label = { Text("productName") }
+                label = { Text("productName") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 50.dp),
+                shape = RoundedCornerShape(20.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
             )
 
             TextField(
                 value = productType,
                 onValueChange = { productType = it },
-                label = { Text("productType") }
+                label = { Text("productType") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 15.dp),
+                shape = RoundedCornerShape(20.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
             )
 
-            Button(onClick = {
-                    nfcId = nfcMethods.writeNfcTag() ?: ""
-            }) {
+            Button(onClick = { nfcId = nfcMethods.writeNfcTag() ?: "" }) {
                 Text("Scan NFC")
             }
             if( nfcId != "") {
@@ -57,7 +86,7 @@ fun AddProduct(
                     Text("Submit")
                 }
             }
-
         }
+
     }
 }
