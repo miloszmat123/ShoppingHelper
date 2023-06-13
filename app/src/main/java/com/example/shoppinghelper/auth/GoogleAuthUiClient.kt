7f3822f26889db88c45915fc794721aef3ccdf7a
Milosz Matuszewski <myyylek@gmail.com>
2023-06-14 00:53:52 +0200

    refactor

--- app/src/main/java/com/example/shoppinghelper/auth/GoogleAuthUiClient.kt
+++ app/src/main/java/com/example/shoppinghelper/auth/GoogleAuthUiClient.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.content.IntentSender
 import com.example.shoppinghelper.R
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.BeginSignInRequest.GoogleIdTokenRequestOptions
 import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
